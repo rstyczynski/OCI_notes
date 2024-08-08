66061a61b0b18c486f0c4c51820706d663fe2ac3
rstyczynski <rstyczynski@gmail.com>
2024-08-08 07:35:06 +0200

    sss

--- security/sss-provide_shares.sh
+++ security/sss-provide_shares.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-: ${shares_working_dir:=$HOME/sss/generate}
-: ${shares_input_dir:=$HOME/sss/input}
+: ${sss_session:=$HOME/sss/generate}
+: ${sss_input:=$HOME/sss/input}
 
 numbers=(1 2 3 4 5)
 shuffled_numbers=$(echo "${numbers[@]}" | tr ' ' '\n' | shuf | tr '\n' ' ')
 
 for share_no in $shuffled_numbers; do
     read -p "Pressn Enter to send secret share no. $share_no."
-    echo -n $(tail -$share_no $shares_working_dir/shares.txt | head -1) > "$shares_input_dir"/share.tmp
-    share_sha=$(sha256sum "$shares_input_dir"/share.tmp | awk '{print $1}')
-    mv "$shares_input_dir"/share.tmp "$shares_input_dir"/$share_sha
+    echo -n $(tail -$share_no $sss_session/shares.txt | head -1) > "$sss_input"/share.tmp
+    share_sha=$(sha256sum "$sss_input"/share.tmp | awk '{print $1}')
+    mv "$sss_input"/share.tmp "$sss_input"/$share_sha
 done
