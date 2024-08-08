5bc5eb2de67c08e413bd8fecf86a8d664b62fe49
rstyczynski <rstyczynski@gmail.com>
2024-08-08 09:49:46 +0200

    manual or generated

--- security/sss-provide_shares.sh
+++ security/sss-provide_shares.sh
@@ -12,23 +12,15 @@ for number in $shuffled_numbers; do
 done
 
 for share_pos in ${numbers[@]}; do
-    echo $share_pos
-    read -p "Provide share, and press Enter" share
+    read -p "Provide share, and press Enter: " share
     if [ ! -z "$share" ]; then
         echo -n $share > "$sss_input"/share.tmp
     else
-        echo "None provided. Taking share from generated ones"
         share_no=${shuffled_array[$(( share_pos - 1))]}
+        
+        echo "None provided. Taking share no. $share_no from generated ones"
         echo -n $(tail -$share_no $sss_session/shares.txt | head -1) > "$sss_input"/share.tmp
     fi
     share_sha=$(sha256sum "$sss_input"/share.tmp | awk '{print $1}')
     mv "$sss_input"/share.tmp "$sss_input"/$share_sha    
-done
-
-
-for share_no in $shuffled_numbers; do
-    read -p "Provide share, and press Enter to send secret share no. $share_no."
-    echo -n $(tail -$share_no $sss_session/shares.txt | head -1) > "$sss_input"/share.tmp
-    share_sha=$(sha256sum "$sss_input"/share.tmp | awk '{print $1}')
-    mv "$sss_input"/share.tmp "$sss_input"/$share_sha
 done
